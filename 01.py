293e5360b0689eac1b0ba0b4e71e3dbace38cee2
ZEM <860961260@qq.com>
2020-04-20 19:05:50 +0800

    第一次向GitHub提交代码

---
+++ 01.py
@@ -0,0 +1,291 @@
+""" 第一天Python的学习 """
+
+# print:打印  可以打印很多种类型数据，逗号隔开就可以
+# print('"Hello,world!"')  #  字符串需 "  " 或 ' ' 无论用单引号或双引号，但要成对出现 括起来
+# print(2333) #整数
+# print(2.333) #小数
+# print(True) #布尔值 用于判断里面 一些语言中还用非0表示真，0表示假
+# print(()) #元组
+# print([]) #数组
+# print({}) #字典
+#Python的注释分 单行注释： #注释内容  多行注释："""  注释内容  """
+# print("Hello,world!",2333,True)
+
+'''
+ 变量的命名
+·变量名只能包含字母、数字和下划线。变量名可以字母或下划线打头，但不能以数字打头，例如，可将变量命名为message_1，但不能将其命名为1_message。
+· 变量名不能包含空格，但可使用下划线来分隔其中的单词。例如，变量名greeting_message可行，但变量名greeting message会引发错误。
+· 不要将Python关键字和函数名用作变量名，即不要使用Python保留用于特殊用途的单词，如print。
+· 变量名应既简短又具有描述性。例如，name比n好，student_name比s_n好，name_length比length_of_persons_name好。
+· 慎用小写字母l和大写字母O，因为它们可能被人错看成数字1和0。
+'''
+# 字符串的拼接（合并），但是只能相同数据类型操作  不能用减号的 可以用乘号
+# first_name = "ada"
+# last_name ="lovelace"
+# full_name = first_name+" "+last_name   # "",引号之间有个空格
+# print("hallo, "+full_name.title()+"!") # .title()将字符串每个单词的首字母改大写  .upper()全部大写 ：.lower()全部小写    hallo，后面有个空格
+# a = input ("请输入：")
+# b = input ("请输入：")
+# print("input获取的内容：",a+b) # 数据类型，形成了字符串的拼接 
+# input的特点：不管你输入什么东西，只要是通过input获取的，对于我们代码来说都是字符串
+# print(((1+2)*3-4)/5) #Python的运算
+# print(2>3)   
+# print(2<3)
+#变量和赋值
+# Xx = "张三" # 把张三这个值赋值给了名字叫xx的这个变量   变量一定要用小写还是最好用小写？
+# print(Xx)
+
+#判断数据类型 Type
+# a = type(2333)
+# print(a) #读取了数据判断其类型为整数（int）  运行结果为：<class 'int'>
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+#数据类型的转换
+# a = int("2333")
+# print(type(a))  #字符串转换为整数
+# a = str(True)
+# print(type(a))  #布尔值转换为字符串
+# print(bool(1)) #整数转换为布尔值
+# print(type(bool(1))) #判断其数据类型
+# print(bool(0))
+"""
+数据类型转换的规律：
+1任何数据都可以转换为字符串；除了 空/None 不能转
+2整数和小数可以互相转换；
+3字符串转换为其他类型的数据，必须满足【长得像】这个规律  （口水话，但有用）。长得像：如：2333、2.333 
+"""
+
+# a = int (input("请输入："))
+# b = int (input("请输入："))
+# print(a+b) #整数的相加
+# a = float (input("请输入："))
+# b = float (input("请输入："))
+# print("input获取的内容是：",a+b) #支持小数的相加
+
+# a = "哈哈哈哈"
+# print(len(a))  # len是用于获取“字符串”的长度的
+# 注意与下面的区分？所输出的长度不同
+
+""" 第二天Python的学习 """
+
+#  元组，下标，正向从0开始编号，反向-1开始
+# a = (1,2,3,4,"好好","学习","Python",True,False,"Python","Python")
+# print(len(a)) 
+# print(a[-6])  
+# a.index("Python") # index查找某一个值的下标
+# index count ，他们只能在一维元组中使用
+# print(a.index("Python")) #当有很多个重复的值的时候，它搜索这个值的下标是第一个值的下标，就近原则
+# print(a.count("Python")) # count统计某个值的数量
+# print("Python" in a) # in 指在  not in 不在
+# print(1 not in a)  #指定的对象判断在不在成员当中，即：成员关系操作符  对象[not]in序列
+# print("\t我要好好学Python\n"*10) #字符串的重复操作： "字符串"*整数
+''' 空格与（制表符\t），通俗点说这两个东西的内部表示方法和显示出来的都不一样，编程的时候，空格用" "表示，制表符用"\t"表示
+    显示的时候空格就是一个空格 制表符一般显示为四个空格，换行符：\n
+'''
+# name= input("Please input your name: ") #字符串的替换功能 %s 替换字符串  %d十进制整数
+# print("Hello, %s good morning!" %name)  #一种字符串格式化的语法， 基本用法是将值插入到 %s占位符的字符串中。%s,表示格式化一个对象为字符 
+# name= str(input("Please input your name: "))
+# print("Hello, "+name+" good morning!" )   #同上
+#二维元组
+# b =(a,"好好学习","天天向上") # b 里面有几个值？  3
+# print(b[0][6])
+#切片[0:整数]
+# print(a[0:4]) # 左闭右开
+# print(a[:4]) #效果同上   不写 0 ，意思从同开始
+# print(a[4:6])#取第4个和第5个
+# print(len(a))  # len是用于获取“字符串”的长度的
+# print(a[6:]) #不写 最后 ，意思从 6 开始到最后
+# print(a[6:12])# 效果同上
+# print(a[:]) # 全部
+
+#列表 （Python里叫列表/list（同数组一样））  与元组区别：元组一旦写好过后不可以修改，而列表是可以修改的
+#列表 list  同样也有成员关系操作符，拼接（连接）操作符，重复操作符和切片操作符  等功能
+# a =[1,2,3,4,"好好","学习","Python",True,False,"Python","Python"]
+# print(a[-6]) 
+# a.append("append1") 
+# a.append("append2") #往列表尾端增加（追加）一个数据
+# b =["呵呵","嘿嘿"]
+# print((a + b)*2)  #拼接和重复
+# b = a.remove("Python") #移除（删除）某个值（就近）
+# print(b)
+# b = a.remove(0)  # 注意：True = 1,False = 0
+# print(a)  
+# a.insert(4,"inster")  #往数组中指定的位置插入数据
+# print(a)
+# a.pop(0)
+# print(a)
+# b = a.pop(0)# 类似于剪切，把数组里某一个值从中拿出来用到别处去
+# print(b)
+# print(a)
+# c = a.pop(0)#剪切数据a，赋值给c用
+# print(b+c)
+# print(a)
+# a.clear() #清空数组
+# print(a)
+# xx =["Hello,Python","Hello,World"]
+# a.extend(xx) #把一个数组里面的多个值加到另一个数组的数据中去，即：合并数组
+# print(a)  
+
+
+# xx = [0,False,1,True]
+# c = xx.count(1) #统计某个值得数量
+# print(c)
+#下标不要超出范围  否则为 越界
+   
+"""
+Python的语法
+所有的方法都是小括号结尾的。比如，print(),input(),len(),... 
+元组、数组、字典的取值，都是用中括号，比如 a[0]
+元组、数组、字典的定义，分别是(),[],{}
+"""  
+
+"""
+字典的特点(记录对应关系)
+1、字典中的值没有顺序。  字典把数组里的下标自定义了
+2、字典的结构必须是 键值对的结构。 key:value(值) {"哈希值":"对象"}
+3、字典的取值，是通过Key去取这个value
+          任何地方的字符串都要加引号
+"""
+# a = {"name":"张三",
+#      0:"哈哈",
+#      "age":25
+#      }
+# print(type(a),a)
+# print (a["name"])  #取值
+# a["high"] = "183cm" #新增
+# print(a)
+# a["name"] = "李四" #修改
+# print(a)
+
+# b = a.get("name") #取值
+# print(b)
+# b = a.pop("name") #剪切
+# print(b)
+# print(a)
+# a.update(name=1111) #更新与修改类似  name为啥没加引号？  name为变量，赋值给name一个值
+# print(a)
+# print(a.get("name"))
+# print(a["name"])
+# print("---------------")
+# #假如写了不存在的 Key  区别就如下：
+# print(a.get("name1")) #会回馈为 空/None
+# print(a["name1"]) # 代码报错           两者虽都为取值，这就是不同点
+
+#数组和字典的删除
+# del a["name"]
+# print(a)
+
+""" 第三天Python的学习 """
+
+#判断  Tab缩进       == 判断是否相等   = 是赋值
+
+
+# print("比大小")
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# if a > b:  # 条件过后不要忘记冒号 ：
+#     print(a) #前面缩进为四个空格，代表和if是一个代码块
+# else:
+#     print(b)
+
+# age = int(input("请输入你的年龄："))
+# if age > 60:               # 60是整数（int），所以age得是整数才行，记得数据类型转换和对应
+#     print("你应该退休了")
+# elif age >30:
+#     print("家庭责任重大")
+# elif age >20:
+#     print("你需要好好思考你想要的生活是什么？") 
+# else:
+#     print("好好学习天天向上") 
+
+""" 
+Python的判断条件 
+三种类型：1、if  表达式： 2、if 表达式：  3、if 表达式：
+                代码块         代码块         代码块 
+                           else：         elif 表达式： #（else-if）
+                               代码块          代码块 
+                                          (可以多个 elif...)
+                                          else：
+                                              代码块           
+"""
+# age = int(input("请输入你的年龄："))
+# # print(type(age))
+# if age == "":
+#     print("不能为空！！！")
+#     exit()
+# if age > 20 and age <30:
+#     print("你该成家立业了！")
+# elif age >30 and age <=50:
+#     print("家庭责任重大") 
+# else:
+#     print("保证生命成长")  
+
+# a = "你好"
+# if a in "你好吗？":    #in  表示：a 的内容存在 in 后  in后可以是字符串也可以是数组、字典等内容
+#     print("我很好")
+# else:
+#     print("I'm Sorry")
+''' 测试时记得特殊值，比如 空 '''
+# a = input("请输入：")
+# if type(a) is int :   # is用来数据判断类型的
+#     print("是整数")
+# elif type(a) is str:
+#     print("是字符串")
+# else:
+#     print("其他") 
+"""
+while循环语句
+while 表达式：
+    代码块
+"""                 
+# a = 1
+# while a < 10:
+#     print("Hello,Python",a)
+#     break # 中断死循环
+#     a = a + 1
+
+# for 循环 原理： 遍历实现 
+
+# a = "你好，今天你吃了吗？"
+# print(len(a)) 
+                 #两者输出len的值不同
+a = "张三","李四","王麻子","浪晋","流云","希希","小梁","二狗"
+# print(len(a))
+for i in a:   # 遍历，每一个都打印一遍  i是一个变量名
+    print(i)
+
+# range()数列生成器  
+# b = list(range(100)) #自动生成了一个数列  左闭右开   只写一个值，默认从0开始依次
+# b = list(range(0,100,2)) # 步进/步长  默认从0开始按步进依次
+# print(b)
+
+for i in range(100):
+    print(i)
+
+# 打印九九乘法表
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print(j,"X",i,"=",i*j,end="  ")  # end 控制不换行
+#     print() # 空的print 起了换行的作用
+
+# 从1到10 所有偶数的平方
+# alist = []
+# for i in range(1,11):
+#     if(i%2==0):
+#         alist.append(i*i)
+# print(alist)
+# #python 独有的 列表（数组）推导式 或字典推导式
+# blist = [i*i for i in range(1,11) if(i%2)==0]
+# print(blist)
+
+""" 第四天Python的学习 """
+for i in range(10):
+    if i ==3 and 4 and 9:
+        continue
+    print(i)
---
+++ 作业练习.py
@@ -0,0 +1,168 @@
+"""
+练习：
+通过代码获取两段内容，并且计算他们的长度的和。
+"""
+# a = input ("请输入：")
+# b = input ("请输入：")
+# print("input获取的内容：",a+b)
+# print(len(a+b))
+
+#答案
+
+#  a = input ("请输入：")
+# b = input ("请输入：")
+# print("两段字符串的长度是：",len(a)+len(b))
+
+"""
+练习：
+获取用户输入的出生年份，判断其生肖。
+"""
+# Symbolic_Animals =( '子鼠','丑牛','寅虎','卯兔','辰龙','巳蛇','午马','未羊','申猴','酉鸡','戌狗','亥猪')
+# # print(len(Symbolic_Animals))
+# year =int(input("请输入你完整的出生年份："))
+# print(Symbolic_Animals[(year - 2008)% 12])
+# # 或者知道其年龄判断属相
+# Symbolic_Animals =( '子鼠','丑牛','寅虎','卯兔','辰龙','巳蛇','午马','未羊','申猴','酉鸡','戌狗','亥猪')
+# Age =int(input("请输入你的年龄："))
+# if Age%12 == 0:
+#     print("恭喜了，今年是你的本命年！")
+# else:
+#     print(Symbolic_Animals[-Age%12])
+
+# 反之，知道其属相判断其年龄   ？？？
+# Symbolic_Animals =( '鼠','牛','虎','兔','龙','蛇','马','羊','猴','鸡','狗','猪')
+# Animals =input("请输入你的属相：")
+# year =int(input("请输入现在完整的年份："))
+# A = Symbolic_Animals.index(Animals) # index查找某一个值的下标
+# # print(Symbolic_Animals.index(Animals))
+# B = str((year-2008)/12+A)
+# print("你的年龄为："+B)
+
+
+
+"""
+练习：
+获取用户输入的个人信息，并且存储到字典中。
+个人信息包括了:Name,Age,Sex。
+"""
+# a ={"Name" : input("请输入你的姓名："),"Age" : input("请输入你的年龄："),"Sex" : input("请输入你的性别：")}
+# print(a)
+
+#答案
+
+# Name = input("请输入你的姓名：")
+# Age = input("请输入你的年龄：")
+# Sex = input("请输入你的性别：")
+# Userinfor ={}
+# Userinfor.update(Name=Name,Age=Age,Sex=Sex) 
+# print(Userinfor)   方法一
+
+# Userinfor["Name"] = Name
+# Userinfor["Age"] = Age
+# Userinfor["Sex"] = Sex
+# print(Userinfor)  方法二
+
+"""
+练习：
+现在有8个学生的成绩，需要录入到系统中。
+这是个人分别是，张三、李四、王麻子、浪晋、流云、希希、小梁、二狗
+并且名字和成绩需要对应上。
+而且大于60的和小于60的需要分开存放。
+"""
+
+# c = 1
+# while c < 9 :
+#     a = input ("请输入学生姓名：")
+#     b = int(input ("请输入该生成绩："))
+#     if (b < 60) :
+#        print(a,b)
+#     else:
+#         a = input ("请输入学生姓名：")
+#         b = input ("请输入该生成绩：")
+#     c = c + 1
+
+# highchengji = {} #定义了一个空字典，用来存储大于60的信息
+# lowchengji = {}  #定义了一个空字典，用来存储小于60的信息
+# studentlist = ["张三","李四","王麻子","浪晋","流云","希希","小梁","二狗"]
+# a = 0  #定义了一个变量，用来控制数组下标的变化
+# while a < len(studentlist) :  #为什么写个循环？因为所有的人的信息的录入，都是要用input，所以写了循环。len判断了数组的长度，总长度-1就是最大的下标
+#     # print(studentlist[a])   # 为了便于自己理解下一句代码   理解为输出studentlist下标为xx的值
+#     chengji = int(input("请输入"+studentlist[a]+"的成绩：")) #？？？？？录入信息，为了方便判断，所以转换了类型
+#     if chengji >= 60: #判断分数
+#         highchengji[studentlist[a]] = chengji #存到字典中
+#     else:
+#         lowchengji[studentlist[a]] = chengji
+#     a = a + 1 #控制下标变化，每一次循环都 +1
+# print("大于60的：",highchengji)
+# print("小于60的：",lowchengji)   #方法一
+
+"""  对  chengji = int(input("请输入"+studentlist[a]+"的成绩："))  该语句的作解
+input（"请输入张三的成绩："）  学生不是固定的一个
+input（"请输入"+"张三"+"的成绩："）  所以拆开来写，字符串相加拼接起来
+进一步理解：
+print(studentlist[a]) = 
+"""
+
+# highchengji = {} #定义了一个空字典，用来存储大于60的信息
+# lowchengji = {}  #定义了一个空字典，用来存储小于60的信息
+# studentlist = ["张三","李四","王麻子","浪晋","流云","希希","小梁","二狗"]
+# for i in studentlist:
+#     chengji = int(input("请输入"+i+"的成绩："))
+#     if chengji >= 60: #判断分数
+#         highchengji[i] = chengji #存到字典中
+#     else:
+#         lowchengji[i] = chengji
+# print("大于60的：",highchengji)
+# print("小于60的：",lowchengji)      #方法二
+
+"""
+练习：
+打印九九乘法表
+"""
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print(j,"X",i,"=",i*j,end="  ")  
+#     print() 
+
+"""
+练习：
+记录生肖，根据年份来判断生肖。
+"""
+# Symbolic_Animals =( '鼠','牛','虎','兔','龙','蛇','马','羊','猴','鸡','狗','猪')
+# for year in range(1990,2021):
+#     print('%s年的生肖是：%s'%(year,Symbolic_Animals[(year - 2008)% 12]))
+''' 一种字符串格式化的语法， 基本用法是将值插入到%s占位符的字符串中。
+    %s,表示格式化一个对象为字符   %s 替换字符串  被替换的内容如何用变量替换进来：%()  括号里是替换为字符串的内容
+'''
+"""
+练习1：
+通过print打印，模拟一个红绿灯的功能，注意红灯30次，绿灯35次，黄灯3次。
+练习2：
+使用代码，实现一个注册的功能。
+用户输入账号和密码，要求账号长度是5-8位，密码6-12位，并且账号不能以下划线开头。
+储到字典中，{username:password}
+"""
+
+"练习一"
+# light = {"红灯":30,"绿灯":35,"黄灯":3}
+# while True: # True 写成 1 也行
+#     for i in light:  # 遍历字典中的 Key值
+#         for j in range(light[i]):  # 第一次循环遍历到红灯时循环30次，绿灯时35次，黄灯时3次
+#             print(i,"倒计时还有：",light[i]-j,"秒") 
+#     break
+
+"练习二"
+# username = input("请注册你的账号名：")
+# password = input("请输入你的密码：")
+# if 5<= len(username) <= 11:
+#     if username[0] not in "-":
+#         if 6<= len(password) <= 12:
+#             print("注册成功!请返回重新登陆")
+#         else:
+#             print("密码必须6-12位")
+#     else:
+#         print("账号的不能以下划线开头！")
+# else:
+#     print("账号的长度不符合规范，请输入5-8位账号")
+
+
